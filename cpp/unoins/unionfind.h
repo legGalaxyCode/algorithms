64434daf76cf19b86b4f4fa93ed6b4d0d287486b
workcat <zhavoronkov.workcat@gmail.com>
2020-09-04 12:28:46 +0300

    union-find alg added

---
+++ cpp/unoins/unionfind.h
@@ -0,0 +1,32 @@
+#ifndef UNIONFIND_H
+#define UNIONFIND_H
+#include<array>
+
+class UF
+{
+public:
+    UF(int n) : N(n) {
+        id = new int [N];
+        for(int i = 0; i < n; i++)
+            id[i] = i;
+    };
+    void unionFind(int p, int q) {
+        unsigned  temp = id[p];
+        id[p] = id[q];
+        id[q] = temp;
+    };
+    bool connected(int p, int q) {
+        if (id[p] == id[q]) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+    int find(int p);
+    int count();
+private:
+    unsigned const N;
+    int* id;
+};
+
+#endif // UNIONFIND_H
